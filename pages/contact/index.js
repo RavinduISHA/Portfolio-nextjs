e07ad6868902a52b21eae038cb512458a5090f18
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2024-05-07 09:11:22 +0530

    update contact mail

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -9,27 +9,36 @@ import { motion } from "framer-motion";
 
 import { fadeIn } from "../../variants";
 
-import emailJS from '@emailjs/browser';
+import emailJS from "@emailjs/browser";
 import { useRef, useState } from "react";
 
 const Contact = () => {
   const form = useRef();
 
-  const [ isSuccess, setIsSuccess ] = useState(false)
-  const [ isError, setIsError ] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailJS.sendForm('service_pci3h9i', 'template_lt535wf', form.current, 'KgJWy3dN2iclHQyNO')
-    .then((result) => {
-      console.log(result.text);
-      setIsSuccess(true);
-    }, (error) => {
-      console.log(error.text);
-      setIsError(true);
-    });
-    e.target.reset()
+    emailJS
+      .sendForm(
+        "service_pci3h9i",
+        "template_lt535wf",
+        form.current,
+        "KgJWy3dN2iclHQyNO"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setIsError(true);
+        }
+      );
+    e.target.reset();
   };
 
   return (
@@ -39,10 +48,10 @@ const Contact = () => {
         <div className="flex flex-col w-full max-w-[700px]">
           {/* text */}
           <motion.h2
-            variants={fadeIn('up', 0.2)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
             className="h2 text-center mb-12"
           >
             Let&apos;s <span className="text-accent">Connect.</span>
@@ -51,25 +60,57 @@ const Contact = () => {
           <motion.form
             ref={form}
             onSubmit={sendEmail}
-            variants={fadeIn('up', 0.4)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
             className="flex-1 flex flex-col gap-6 w-full mx-auto"
           >
             <div className="flex gap-x-6 w-full">
-              <input type="text" placeholder="name" className="input" name="name" required/>
-              <input type="email" placeholder="email" className="input" name="email" required/>
+              <input
+                type="text"
+                placeholder="name"
+                className="input"
+                name="name"
+                required
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="Input"
+                name="email"
+                required
+              />
             </div>
-            <input type="text" placeholder="Subject" className="input" name="subject" />
-            <textarea className="textarea" placeholder="Message" name="message" required></textarea>
+            <input
+              type="text"
+              placeholder="Subject"
+              className="input"
+              name="subject"
+            />
+            <textarea
+              className="textarea"
+              placeholder="Message"
+              name="message"
+              required
+            ></textarea>
             <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
-              <span className="group-hover:translate-y-[120%] group-hover:opacity-0 transition-all duration-500">Let&apos;s talk</span>
-              <BsArrowRight className="translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"/>
+              <span className="group-hover:translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                Let&apos;s talk
+              </span>
+              <BsArrowRight className="translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
           </motion.form>
-          {isSuccess && <span className="p-4 mt-2 mb-4 text-sm text-green-100 rounded-lg bg-green-500">Thanks for contacting me!, </span>}
-          {isError && <span className="p-4 mt-2 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">An error occurred. Please try again.</span>}
+          {isSuccess && (
+            <span className="p-4 mt-2 mb-4 text-sm text-green-100 rounded-lg bg-green-500">
+              Thanks for contacting me!,{" "}
+            </span>
+          )}
+          {isError && (
+            <span className="p-4 mt-2 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
+              An error occurred. Please try again.
+            </span>
+          )}
         </div>
       </div>
     </div>
